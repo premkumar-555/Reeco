852bbe2b2efb025373bc4b51914da75de1607095
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-10-20 15:59:42 +0530

    created order header component

--- task/src/components/Home/components/OrderHeader.jsx
+++ task/src/components/Home/components/OrderHeader.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
-import { Box, Text, Icon, Circle    } from '@chakra-ui/react'
+import { Box, Text, Icon , Button   } from '@chakra-ui/react'
+import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
 const OrderHeader = () => {
   return (
     <Box display='flex' justifyContent='space-between' alignItems='center' w='100%'
-    h='100px' px='100px' zIndex='2' boxShadow='lg' py='15px' border='1px solid none'>
-     <Box h='100%' display='flex' flexDirection='column' justifyContent='space-between' alignItems='center'>
-      <Box border='1px solid black'>
-        <Text fontSize='sm' fontWeight='500'></Text>
+    h='100px' px='100px' zIndex='2' boxShadow='lg' py='10px' border='1px solid blue'>
+     <Box w='18%' h='100%' display='flex' flexDirection='column' justifyContent='space-between' alignItems='center'>
+      <Box w='70%' display='flex' flex='1' justifyContent='space-between' alignItems='center' >
+        <Box  display='flex' textAlign='left'>
+            <Text textAlign='left' fontSize='sm' fontWeight='500'>Orders</Text>
+            <Icon as={MdOutlineKeyboardArrowRight   } w={5} h={5} color='red.500' mt='1px'/>
+        </Box>
+        <Box >
+            <Text fontSize='sm' fontWeight='500' as='u'>Order 32457ABC</Text>
+        </Box>
       </Box>
-      <Box border='1px solid black'>
-        <Text fontSize='24px' fontWeight='900' >Order 32457ABC</Text>
+       <Box w='70%' display='flex' flex='1' justifyContent='space-between' alignItems='center' >
+        <Box>
+            <Text fontSize='22px' fontWeight='600'>Order</Text>
+        </Box>
+        <Box >
+            <Text fontSize='22px' fontWeight='600'>32457ABC</Text>
+        </Box>
       </Box>
      </Box>
-     <Box></Box>
-    </Box>
+      <Box h='100%'  w='17%' display='flex' justifyContent='space-between'  alignItems='flex-end'>
+       <Button size='md' borderRadius='full' border='1px solid #1B5E20' bg='white' color='#1B5E20'>
+        Back
+       </Button>
+       <Button size='md' borderRadius='full' bg='#1B5E20' color='white'>
+        Approve order
+     </Button>
+      </Box>
+     </Box>
   )
 }
 
